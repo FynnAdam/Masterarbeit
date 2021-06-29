ea9f945cbe56413784e311729bb95740e8b29ca8
FynnAdam <84462127+FynnAdam@users.noreply.github.com>
2021-06-29 11:59:46 +0200

    Add files via upload

---
+++ Exp2.py
@@ -0,0 +1,77 @@
+from NeuronalNetwork import *
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import multiprocessing as mp
+import math
+
+#Model abhängig von n
+#trainiere Model in Funktion um zu Parallelisierung
+# n: Größe des Trainingsampls
+def netz(n):
+    # erzeuge zufällig lineare Regressionsfunktion mit Dimension d und und Summanden mit Hölderkonstante K, beta =1
+    d = 2
+    K = 15
+    f_coeff = list(tuple(map(lambda x: K, list(range(1, d + 1)))))
+    f0 = lambda *x: sum(list(map(lambda x1, x2: x1 * x2, *x, f_coeff)))
+
+    # Datensample (trainingsdatensatz) mit std.norm.vert. Fehler
+    n_train = n
+    x = np.random.uniform(0, 1, size=(n_train, d))
+    y = noisy_sig(np.fromiter(map(f0, x), dtype=np.int), 1)
+
+    n_test = 5000
+    x_orig = np.random.uniform(0, 1, size=(n_test, d))
+    y_orig = np.fromiter(map(f0, x_orig), dtype=np.int)
+
+    # Parameter neuronales Netz
+    L = math.ceil((2 * np.log2(3 * 2 * d) * np.log2(n_train)))
+    p_i = math.ceil(n_train ** (1 / 3))
+    p = list(tuple(map(lambda x: p_i, list(range(1, L + 1)))))
+    s = math.ceil(20*n_train ** (1 / 3) * np.log(n_train))
+    p.append(1)
+    p.insert(0, d)
+    # netzwerk
+    NN1 = NeuronalNetwork(L, p, s)
+    NN1.NN.compile("adam", 'mse', tf.keras.metrics.MeanSquaredError())
+    NN1.NN.summary()
+    NN1.fit(x, y, epochs=100, batch=100)
+
+    y_pred = NN1.NN.predict(x_orig)
+    NN1.NN.evaluate(x_orig, y_orig)
+
+    # Analysiere Netz/zähle nullen
+
+    weights = NN1.NN.get_weights()
+    max_abs_weight = max(abs(min(weights).sum()), abs(max(weights).sum()))
+    F = max((K + 1) * d, (p_i + 1) * L * max_abs_weight)
+    T = sum([(p[i-1]+1)*p[i] for i in range(1,L+2,1)])-1
+
+    # speichere Daten
+    mse = tf.keras.losses.MeanSquaredError()
+    return [n_train, n_test, L, s, T, p_i,
+                        mse(y_orig, y_pred).numpy() / len(y_orig), d, K, F,
+                        n_train ** (1 / 3) * np.log(n_train),
+                        (n ** (-2 / 3)) * (np.log(n)) ** 3, max_abs_weight]
+
+
+
+if __name__ == '__main__':
+    pool = mp.Pool(3)
+    list_iterable = list(range(100,251,10))+list(range(300, 501, 50))+list(range(600,1501,100))
+    results = [pool.apply_async(netz, args = (n,)) for n in list_iterable*100]
+    results = [result.get() for result in results]
+    pool.close()
+    df = pd.DataFrame(results, columns=['n_train','n_test','L','s','anzahl Gewichte gesamt','p_i','mean_squared_error_test','d','K','geschätztes F','s erwartet','Schranke nach Theorem','maximales Gewicht'])
+    writer = pd.ExcelWriter('output.xlsx')
+    df.to_excel(writer)
+    writer.save()
+    writer.close()
+
+
+
+
+
+
+
+
---
+++ NeuronalNetwork.py
@@ -0,0 +1,83 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.python.keras import activations
+from DenseNew import SparseDense
+from DenseNew import OutputDense
+
+# Klasse für neuronales Netz mit Tiefe L, Schichten p, anzahl nicht-null Parameter s (im EW)
+class NeuronalNetwork():
+    # sequential deep network
+
+  def __init__(self,L,p,s,**kwargs):
+    super(NeuronalNetwork, self).__init__(**kwargs)
+      # L: Tiefe des Netzes (Anzahl verdeckte Schicheten)
+      # p: Knoten je Schicht (Vektor mit L+2 Einträgen)
+      # s: nicht-null Parameter (Zahl zwischen >>1 und Anzahl Gesamtparameter)
+
+
+    [d,*hidden,output] = p
+    T = sum([(p[i-1]+1)*p[i] for i in range(1,L+2,1)])-output # Anzahl Gesamtparameter
+    T=T-d*hidden[0]-hidden[-1]
+    s= s-d*hidden[0]-hidden[-1]-L # Ausgleich, da Gewichte der ersten und letzte Schicht wird im Netz auf trainierbar gesetzt
+
+    self.NN = tf.keras.Sequential()
+
+    # Eingabeschicht
+    self.NN.add(tf.keras.layers.InputLayer(input_shape=(d,)))
+
+    # 1. verdeckte Schicht
+    matW = np.random.binomial(1, 1, d*hidden[0]).reshape((d, hidden[0]))
+    vecB = np.random.binomial(1, min(1,s / T), hidden[0])
+    self.NN.add(SparseDense(matW=matW, vecB=vecB, activation=activations.relu))
+
+    # verdeckte Schichten
+    for width in hidden[:-1]:
+        matW = np.random.binomial(1,min(1,s / T),width*width).reshape((width,width))
+        matW[0][0] = 1
+        vecB = np.random.binomial(1,min(1,s / T),width)
+        self.NN.add(SparseDense(matW=matW,vecB=vecB,activation = activations.relu))
+
+    # letzte verdeckte Schicht
+    matW = np.random.binomial(1, 1, hidden[-1]).reshape((hidden[-1], 1))
+    self.NN.add(OutputDense(matW=matW, activation=activations.relu))
+
+  def compile(self,optimizer,loss,metric):
+      self.NN.compile(loss=loss, optimizer=optimizer, metrics=metric)
+
+  def fit(self,dataX,dataY,epochs,batch):
+      self.NN.fit(dataX,dataY,epochs=epochs,batch_size=batch,use_multiprocessing=False)
+      weights_fit = [np.array(max(min(w, 1),-1), dtype=np.float32).reshape(w.shape) for w in self.NN.get_weights()]
+      self.NN.set_weights(weights_fit)
+
+class NeuronalNetwork_standard():
+    # sequential deep network
+
+  def __init__(self,L,p,**kwargs):
+    super(NeuronalNetwork_standard, self).__init__(**kwargs)
+      # L: Tiefe des Netzes (Anzahl verdeckte Schicheten)
+      # p: Knoten je Schicht
+
+    [d,*hidden,output] = p
+    T = sum([(p[i-1]+1)*p[i] for i in range(1,L+2,1)])-output # Anzahl Gesamtparameter
+    T=T-d*hidden[0]-hidden[-1]
+
+    self.NN = tf.keras.Sequential()
+    # Eingabeschicht
+    self.NN.add(tf.keras.layers.InputLayer(input_shape=(d,)))
+    # verdeckte Schichten
+    for width in hidden:
+        self.NN.add(tf.keras.layers.Dense(width,'relu',kernel_initializer=tf.keras.initializers.random_uniform(0,0.05),bias_initializer=tf.keras.initializers.zeros))
+    # Ausgabeschicht
+    self.NN.add(tf.keras.layers.Dense(1))
+
+  def compile(self,optimizer,loss,metric):
+      self.NN.compile(loss=loss, optimizer=optimizer, metrics=metric)
+
+  def fit(self,dataX,dataY,epochs,batch):
+      self.NN.fit(dataX,dataY,epochs=epochs,batch_size=batch,use_multiprocessing=False)
+
+def sig(a,b,size):
+    return np.random.uniform(a,b,size=size) # uniform distributed
+
+def noisy_sig(x,sigma):
+    return x + np.random.normal(0,sigma,len(x)) #error
